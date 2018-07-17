a3a9cc4b72ff2a7a1f142e7925b9783f5d08d669
khalid jamal <kh.jamal95@gmail.com>
2018-07-17 22:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ let randomNumber = Math.floor(Math.random()*20)+1
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID = Math.floor(Math.random()*1000000000)+1
+let randomUserID = Math.floor(Math.random()+1)+1000000000
 
 
