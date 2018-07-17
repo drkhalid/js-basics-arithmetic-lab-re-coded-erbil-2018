01409646dc291a4d4f062abff02e3a55ba824ebb
khalid jamal <kh.jamal95@gmail.com>
2018-07-17 22:38:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ let newID =oldID + 1000000000
 
 let ageIsValid=Number.isInteger(currentAge)
 
-const randomNumber = Math.floor(Math.random()*20)+1
+let randomNumber = Math.floor(Math.random()*20)+1
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID = Math.floor(Math.random()+1)+1000000000
+let randomUserID = randomNumber+1000000000
 
